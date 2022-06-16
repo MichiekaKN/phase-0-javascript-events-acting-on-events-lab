1ec3719f7256a4010ba695a3690a4506018fd664
Kelvin Michieka <96nyakundikelvin@gmail.com>
2022-06-16 06:51:31 +0300

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Your code here
-var dodger = document.getElementById('dodger');
+const dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '');
-  var left = parseInt(leftNumbers, 10)
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10)
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
 }
 
 function moveDodgerRight() {
-  var leftNumbers = dodger.style.left.replace('px', '');
-  var left = parseInt(leftNumbers, 10)
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10)
   if (left < 360) {
     dodger.style.left = `${left + 1}px`
   }
